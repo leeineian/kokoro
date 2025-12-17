60cb50bf35389ac0ba44dd41d73e3494d9134c41
leeineian <86507806+leeineian@users.noreply.github.com>
2025-12-17 20:59:33 +0800

    chore: Move deploy-commands.js to scripts directory and update package.json.

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, '../commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
